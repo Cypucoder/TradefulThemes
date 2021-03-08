8b6c1c5c9a90287684774bd5a3d4c1e09811c506
Cypucoder <cypucoder@gmail.com>
2021-03-08 04:38:48 -0500

    Added Angular support, updated routes to serve angular, created API to support angular front end

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MessageController;
+use App\Models\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,35 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/themelist', function() {
+    return Message::all();
+});
+
+//Route::get('/message/{id}/{success}', [MessageController::class, 'view']);
+
+Route::post('/create', function(Request $request){
+    error_log($request);
+    return Message::create([
+        'title' => $request->title,
+        'content' => $request->content,
+        'color1' => $request->color1,
+        'color2' => $request->color2,
+        'color3' => $request->color3,
+        'color4' => $request->color4,
+        'color5' => $request->color5,
+        ]);
+});
+
+Route::put('/update', function(Request $request) {
+    $theme = Message::findOrFail($request->id);
+    $theme->update($request->all());
+});
+
+/*Route::delete('articles/{id}', function($id) {
+    Article::find($id)->delete();
+
+    return 204;
+})*/
